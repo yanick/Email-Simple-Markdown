cbe8959c31c59840848eebddb812df2a68eeeec2
Yanick Champoux <yanick@cpan.org>
2012-05-16 21:33:45 -0400

    documentation

--- lib/Email/Simple/Markdown.pm
+++ lib/Email/Simple/Markdown.pm
@@ -1,4 +1,37 @@
 package Email::Simple::Markdown;
+# ABSTRACT: simple email creation with auto text and html multipart body
+
+=head1 SYNOPSIS
+
+    use Email::Simple::Markdown;
+
+    my $email = Email::Simple::Markdown->create(
+        header => [
+            From    => 'me@here.com',
+            To      => 'you@there.com',
+            Subject => q{Here's a multipart email},
+        ],
+        body => '[this](http://metacpan.org/search?q=Email::Simple::Markdown) is *amazing*',
+    );
+
+    print $email->as_string;
+
+=head1 DESCRIPTION
+
+I<Email::Simple::Markdown> behaves almost exactly like L<Email::Simple>,
+excepts for one detail: when its method C<as_string()> is invoked, the
+returned string representation of the email has multipart body with a 
+I<text/plain> element (the original body), and a I<text/html> element,
+the markdown rendering of the text body.
+
+The markdown convertion is done using L<Text::MultiMarkdown>.
+
+=head1 METHODS
+
+I<Email::Simple::Markdown> inherits all the methods if L<Email::Simple>. 
+In addition, it provides one more method: I<with_markdown>.
+
+=cut
 
 use strict;
 use warnings;
@@ -7,6 +40,13 @@ use Email::Abstract;
 use Text::MultiMarkdown qw/ markdown /;
 
 use parent 'Email::Simple';
+
+=head2 with_markdown()
+
+Returns an L<Email::Abstract> representation of the email, with 
+its multipart body.
+
+=cut
 
 sub with_markdown {
     my $self = shift;
@@ -32,13 +72,10 @@ sub with_markdown {
         )
     ]);
 
-    $DB::single = 1;
-
     return Email::Abstract->new($mail);
 }
 
 sub as_string {
-    $DB::single = 1;
     return $_[0]->with_markdown->as_string;
 }
 
--- t/basic.t
+++ t/basic.t
@@ -1,9 +1,22 @@
 use strict;
 use warnings;
 
-use Test::More tests => 1;
+use Test::More tests => 2;
 
-use Test::Email;
+use Email::Simple::Markdown;
 
+my $email = Email::Simple::Markdown->create(
+    header => [
+        From    => 'me@here.com',
+        To      => 'you@there.com',
+        Subject => q{Here's a multipart email},
+    ],
+    body => '[this](http://metacpan.org/search?q=Email::Simple::Markdown) is *amazing*',
+);
 
+my $text = $email->as_string;
+
+isa_ok $email->with_markdown, 'Email::Abstract';
+
+like $text, qr#<em>amazing</em>#, 'html is present';
 
