8052aba20ede23149481968aee9288cda49c0b0e
Yanick Champoux <yanick@cpan.org>
2012-05-16 21:37:30 -0400

    Build results of cbe8959 (on master)

--- lib/Email/Simple/Markdown.pm
+++ lib/Email/Simple/Markdown.pm
@@ -1,37 +1,12 @@
 package Email::Simple::Markdown;
+BEGIN {
+  $Email::Simple::Markdown::AUTHORITY = 'cpan:YANICK';
+}
+{
+  $Email::Simple::Markdown::VERSION = '0.1.0';
+}
 # ABSTRACT: simple email creation with auto text and html multipart body
 
-=head1 SYNOPSIS
-
-    use Email::Simple::Markdown;
-
-    my $email = Email::Simple::Markdown->create(
-        header => [
-            From    => 'me@here.com',
-            To      => 'you@there.com',
-            Subject => q{Here's a multipart email},
-        ],
-        body => '[this](http://metacpan.org/search?q=Email::Simple::Markdown) is *amazing*',
-    );
-
-    print $email->as_string;
-
-=head1 DESCRIPTION
-
-I<Email::Simple::Markdown> behaves almost exactly like L<Email::Simple>,
-excepts for one detail: when its method C<as_string()> is invoked, the
-returned string representation of the email has multipart body with a 
-I<text/plain> element (the original body), and a I<text/html> element,
-the markdown rendering of the text body.
-
-The markdown convertion is done using L<Text::MultiMarkdown>.
-
-=head1 METHODS
-
-I<Email::Simple::Markdown> inherits all the methods if L<Email::Simple>. 
-In addition, it provides one more method: I<with_markdown>.
-
-=cut
 
 use strict;
 use warnings;
@@ -41,12 +16,6 @@ use Text::MultiMarkdown qw/ markdown /;
 
 use parent 'Email::Simple';
 
-=head2 with_markdown()
-
-Returns an L<Email::Abstract> representation of the email, with 
-its multipart body.
-
-=cut
 
 sub with_markdown {
     my $self = shift;
@@ -80,3 +49,63 @@ sub as_string {
 }
 
 1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Email::Simple::Markdown - simple email creation with auto text and html multipart body
+
+=head1 VERSION
+
+version 0.1.0
+
+=head1 SYNOPSIS
+
+    use Email::Simple::Markdown;
+
+    my $email = Email::Simple::Markdown->create(
+        header => [
+            From    => 'me@here.com',
+            To      => 'you@there.com',
+            Subject => q{Here's a multipart email},
+        ],
+        body => '[this](http://metacpan.org/search?q=Email::Simple::Markdown) is *amazing*',
+    );
+
+    print $email->as_string;
+
+=head1 DESCRIPTION
+
+I<Email::Simple::Markdown> behaves almost exactly like L<Email::Simple>,
+excepts for one detail: when its method C<as_string()> is invoked, the
+returned string representation of the email has multipart body with a 
+I<text/plain> element (the original body), and a I<text/html> element,
+the markdown rendering of the text body.
+
+The markdown convertion is done using L<Text::MultiMarkdown>.
+
+=head1 METHODS
+
+I<Email::Simple::Markdown> inherits all the methods if L<Email::Simple>. 
+In addition, it provides one more method: I<with_markdown>.
+
+=head2 with_markdown()
+
+Returns an L<Email::Abstract> representation of the email, with 
+its multipart body.
+
+=head1 AUTHOR
+
+Yanick Champoux <yanick@babyl.dyndns.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Yanick Champoux.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
