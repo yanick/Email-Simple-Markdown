7694f86ac16b95266a8428bbbfeb4cf56a265b8e
Yanick Champoux <yanick@cpan.org>
2017-02-23 10:21:47 -0500

    split pod into its own file

--- lib/Email/Simple/Markdown.pm
+++ lib/Email/Simple/Markdown.pm
@@ -1,37 +1,6 @@
 package Email::Simple::Markdown;
 # ABSTRACT: simple email creation with auto text and html multipart body
 
-=head1 SYNOPSIS
-
-    use Email::Simple::Markdown;
-
-    my $email = Email::Simple::Markdown->create(
-        header => [
-            From    => 'me@here.com',
-            To      => 'you@there.com',
-            Subject => q{Here's a multipart email},
-        ],
-        body => '[this](http://metacpan.org/search?q=Email::Simple::Markdown) is *amazing*',
-    );
-
-    print $email->as_string;
-
-=head1 DESCRIPTION
-
-I<Email::Simple::Markdown> behaves almost exactly like L<Email::Simple>,
-excepts for one detail: when its method C<as_string()> is invoked, the
-returned string representation of the email has multipart body with a 
-I<text/plain> element (the original body), and a I<text/html> element,
-the markdown rendering of the text body.
-
-The markdown convertion is done using L<Text::MultiMarkdown>.
-
-=head1 METHODS
-
-I<Email::Simple::Markdown> inherits all the methods if L<Email::Simple>. 
-In addition, it provides one more method: I<with_markdown>.
-
-=cut
 
 use strict;
 use warnings;
@@ -47,35 +16,6 @@ use Module::Runtime qw/ use_module /;
 
 use parent 'Email::Simple';
 
-=head2 create( ... ) 
-
-Behaves like L<Email::Simple>'s C<create()>, but accepts the following
-additional arguments:
-
-=over
-
-=item markdown_engine => $module
-
-See C<markdown_engine_set>. If not given, defaults to C<auto>.
-
-=item css => $stylesheet
-
-If provided, the html part of the email will be prepended with the given
-stylesheet, wrapped by a I<css> tag.
-
-=item pre_markdown_filter => sub { ... }
-
-See C<pre_markdown_filter_set>.
-
-=item charset => $charset
-
-The character set supplied to C<Email::MIME::create()>. By default, no character set 
-is passed.
-
-=back
-
-
-=cut
 
 sub create {
     my ( $self, %arg ) = @_;
@@ -98,26 +38,11 @@ sub create {
     return $email;
 }
 
-=head2 markdown_engine
 
-Returns the markdown engine used by the object.
-
-=cut
-
-# TODO split pod and code
 # TODO Moo'fy this base
 
 sub markdown_engine { return $_[0]->{markdown_engine} };
 
-=head2 markdown_engine_set( $module )
-
-Sets the markdown engine to be used by the object. 
-Currently accepts C<auto>, L<Text::MultiMarkdown> or L<Text::Markdown>.
-
-If not specified or set to C<auto>, the object will use the first markdown module it finds,
-in the order given in the previous paragraph.
-
-=cut
 
 our @SUPPORTED_ENGINES = qw/ Text::MultiMarkdown Text::Markdown /;
 
@@ -148,37 +73,9 @@ sub _markdown {
     return $self->{markdown_object}->markdown($text);
 }
 
-=head2 css
-
-Returns the cascading stylesheet that is applied to the html part of the
-email.
-
-=cut
 
 sub css { return $_[0]->{markdown_css} };
 
-=head2 css_set( $stylesheet )
-
-Sets the cascading stylesheet for the html part of the email to be
-I<$stylesheet>.  
-
-    $email->css_set( <<'END_CSS' );
-        p   { color: red; }
-        pre { border-style: dotted; }
-    END_CSS
-
-The I<$stylesheet> can also be an array ref, holding key/value pairs where
-the key is the css selector and the value the attached style. For example, 
-the equivalent call to the one given above would be:
-
-    $email->css_set([
-        p   => 'color: red;',
-        pre => 'border-style: dotted;',
-    ]);
-
-
-
-=cut
 
 sub css_set {
     my( $self, $css ) = @_;
@@ -199,18 +96,6 @@ sub css_set {
     return $self;
 }
 
-=head2 pre_markdown_filter_set( sub{ ... } );
-
-Sets a filter to be run on the body before the markdown transformation is
-done. The body will be passed as C<$_> and should be modified in-place.
-
-E.g., to add a header to the email:
-
-    $mail->pre_markdown_filter_set(sub {
-        s#^#<div id="header">My Corp <img src='..' /></div>#;
-    });
-
-=cut
 
 sub pre_markdown_filter_set {
     my ( $self, $sub ) = @_;
@@ -218,11 +103,6 @@ sub pre_markdown_filter_set {
     return $self;
 }
 
-=head2 charset_set( $charset )
-
-Sets the charset to be used by the email.
-
-=cut
 
 sub charset_set {
     my( $self, $charset ) = @_;
@@ -232,12 +112,6 @@ sub charset_set {
 }
 
 
-=head2 with_markdown()
-
-Returns an L<Email::Abstract> representation of the email, with 
-its multipart body.
-
-=cut
 
 sub with_markdown {
     my $self = shift;
